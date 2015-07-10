15d3913fcfa369264fddcf89a421aff3cfb91226
Poorna Chandra <PG042344@WIN1508018.northamerica.cerner.net>
2015-07-10 14:13:10 +0530

    Html and javascript

--- Crossword/WebContent/myscripts.js
+++ Crossword/WebContent/myscripts.js
@@ -1,6 +1,6 @@
 function addRow() {
           
-    var question = document.getElementById("question");
+    /*var question = document.getElementById("question");
     var answer = document.getElementById("answer");
     var cellRow = document.getElementById("cellRow");
     var cellCol = document.getElementById("cellCol");
@@ -21,8 +21,17 @@ function addRow() {
     row.insertCell(2).innerHTML= question.value;
     row.insertCell(3).innerHTML= answer.value;
     row.insertCell(4).innerHTML= cellData;
-    row.insertCell(5).innerHTML= direction;
- 
+    row.insertCell(5).innerHTML= direction;*/
+	var table = document.getElementById("myTableData");
+
+    var rowCount = table.rows.length;
+    var row = table.insertRow(rowCount);
+	row.insertCell(0).innerHTML= '<input type="checkbox" id="select" >';
+	row.insertCell(1).innerHTML= '<input type="text" name="questionValue" id="questionValue" size="1" required>';
+    row.insertCell(2).innerHTML= '<input type="text" name="answerValue" id="answerValue" required>';
+    row.insertCell(3).innerHTML= '<input type="text" name="answerValue" id="answerValue" required>';
+    row.insertCell(4).innerHTML= '<input type="text" name="answerValue" id="answerValue" size="6" required>';
+    row.insertCell(5).innerHTML= '<input type="text" name="directionValue" id="directionValue" size="2" maxlength=1 required >';
 }
  
 function deleteRow(obj) {
@@ -38,32 +47,7 @@ function deleteRow(obj) {
             rowCount--;
             i--;
         }
-    }  
-}
- 
-function addTable() {
-      
-    var myTableDiv = document.getElementById("myDynamicTable");
-      
-    var table = document.createElement('TABLE');
-    table.border='1';
-    
-    var tableBody = document.createElement('TBODY');
-    table.appendChild(tableBody);
-      
-    for (var i=0; i<3; i++){
-       var tr = document.createElement('TR');
-       tableBody.appendChild(tr);
-       
-       for (var j=0; j<4; j++){
-           var td = document.createElement('TD');
-           td.width='75';
-           td.appendChild(document.createTextNode("Cell " + i + "," + j));
-           tr.appendChild(td);
-       }
-    }
-    myTableDiv.appendChild(table);
-    
+    } 
 }
  
 function load() {
